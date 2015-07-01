c15a57140de3729788df171e730362e49162d75d
Owen Dismuke <owen.dismuke@gmail.com>
2015-07-01 12:16:44 -0700

    Stack completed

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,22 +1,20 @@
 var Stack = function(){
   var someInstance = {};
-  // 
-
 
   // Use an object with numeric keys to store values
-  // {1:'a', 2:'b'}
   var storage = {};
   var key = 0;
 
-  // Implement the methods below
   someInstance.push = function(value){
-    // increment key, and use that as key
-    // store passed in value in 'storage'
     key++;
     storage[key] = value;
   };
 
   someInstance.pop = function(){
+    var result = storage[key];
+    delete storage[key];
+    key--;
+    return result;
   };
 
   someInstance.size = function(){
@@ -24,6 +22,5 @@ var Stack = function(){
   };
 
   return someInstance;
-
 };
  
